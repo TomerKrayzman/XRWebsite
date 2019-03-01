c32f8a3a825cac4a194f0b57b872b6239ca57a78
AbiSri <abiram.sritharan@gmail.com>
2019-03-01 18:19:17 -0500

    Added database for Events and simple admin page

--- XRWebsite/xrhome/models.py
+++ XRWebsite/xrhome/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+	title = models.CharField(max_length=200)
+	desc = models.CharField(max_length=500)
+	start_date = models.DateTimeField('Date of Event')
+	location = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.title
